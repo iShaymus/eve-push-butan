90baa8b35153cf784ddeb440eb3d5b9b4c38bc13
iShaymus <shay.collings@gmail.com>
2024-01-01 09:47:37 +1300

    Added .env variables

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,9 @@
-CREATE DATABASE db;
-\connect db;
+-- Delete default database
+DROP DATABASE postgres;
+
+-- Create schemas in new database
 CREATE SCHEMA app;
 CREATE SCHEMA app_audit;
+
+-- Delete the default schema
+DROP SCHEMA public;
--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,16 @@
 import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const name = process.env.SERVER_NAME;
+const port = process.env.SERVER_PORT || 5000; // default to port 5000 if not specified in .env
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`PushButan Server running on port:${port}`)
+    console.log(`${name} server running on port:${port}`)
 })
